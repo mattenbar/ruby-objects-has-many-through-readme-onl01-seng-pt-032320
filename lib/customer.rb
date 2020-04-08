19d003c0f974ced7571ce7cf5949ab7c04a36a96
matthew enbar <matthew.enbar@gmail.com>
2020-04-08 18:43:19 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,19 +15,25 @@ class Customer
 
   def new_meal(waiter, total, tip=0)
    Meal.new(waiter, self, total, tip)
- end
+  end
 
- def meals
-   Meal.all.select do |meal|
-     meal.customer == self
-   end
- end
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
 
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
 
- def waiters
-   meals.map do |meal|
-     meal.waiter
-   end
- end
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
+  end
 
 end
