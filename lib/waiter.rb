929d859ddc8fa3e9d50f5f0406325a32703355a9
matthew enbar <matthew.enbar@gmail.com>
2020-04-08 18:44:48 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,9 +19,9 @@ class Waiter
   end
 
   def meals
-  Meal.all.select do |meal|
-    meal.customer == self
+    Meal.all.select do |meal|
+      meal.waiter == self #checking for waiter now
+    end
   end
-end
 
 end
